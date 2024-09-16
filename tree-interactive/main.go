2d50639a3229fd831023ad712085b6d9b38295be
tkhashi <pupodenews@gmail.com>
2024-09-16 18:06:52 +0900

    format and remove unused mod

--- tree-interactive/main.go
+++ tree-interactive/main.go
@@ -1,44 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "io/fs"
-    "log"
-    "os"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
 )
 
 func main() {
-    pwd := getCurrentPath()
-    listFiles(pwd)
 }
 
 // 現在のパスを返す
 func getCurrentPath() string {
-    pwd, err := os.Getwd()
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    fmt.Println(pwd)
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(pwd)
 
-    return pwd
+	return pwd
 }
-
 
 // dir配下のファイル、ディレクトリを返す
 func listFiles(dir string) []string {
-    root := os.DirFS(dir)
+	root := os.DirFS(dir)
 
-    mdFiles, err := fs.Glob(root, "*")
+	mdFiles, err := fs.Glob(root, "*")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    if err != nil {
-       log.Fatal(err)
-    }
-
-    var files []string
-    for _, v := range mdFiles {
-       files = append(files,  v)
-       fmt.Println(v)
-    }
-    return files
+	var files []string
+	for _, v := range mdFiles {
+		files = append(files, v)
+		fmt.Println(v)
+	}
+	return files
 }
