df31e7dbf25c38f9855658840b2ae4e8ff07d403
tkhashi <pupodenews@gmail.com>
2024-09-16 17:06:47 +0900

    format

--- bubbletea-tutorial/main.go
+++ bubbletea-tutorial/main.go
@@ -8,76 +8,75 @@ import (
 )
 
 type model struct {
-  choices []string
-  cursor int
-  selected map[int]struct{}
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
 }
 
-
 func initialModel() model {
-  return model{
-    choices: []string{"Buy carrots","Buy celery","Buy kohlrabi",},
-    selected: make(map[int]struct{}),
-  }
+	return model{
+		choices:  []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+		selected: make(map[int]struct{}),
+	}
 }
 
 func (m model) Init() tea.Cmd {
-  return nil
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-  switch msg := msg.(type) {
-  case tea.KeyMsg:
-    switch msg.String() {
-    case "q":
-      return m, tea.Quit
-    case "k":
-      if m.cursor > 0 {
-	m.cursor--
-      }
-    case "j":
-      if m.cursor < len(m.choices)-1 {
-	m.cursor++
-      }
-    case "enter", " ":
-      _, ok := m.selected[m.cursor]
-      if ok {
-	delete(m.selected, m.cursor)
-      } else {
-	m.selected[m.cursor] = struct{}{}
-      }
-    }
-  }
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q":
+			return m, tea.Quit
+		case "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
 
-  return m, nil
+	return m, nil
 }
 
 func (m model) View() string {
-  s := "the header\n\n"
+	s := "the header\n\n"
 
-  for i, choice := range m.choices {
-    cursor := " "
-    if m.cursor == i {
-      cursor = ">"
-    }
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
 
-    checked := " "
-    if _, ok := m.selected[i]; ok {
-      checked = "x"
-    }
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
 
-    s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-  }
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
 
-  s += "\nPress q to quit. \n"
+	s += "\nPress q to quit. \n"
 
-  return s
+	return s
 }
 
 func main() {
-  p := tea.NewProgram(initialModel())
-  if _, err := p.Run(); err != nil {
-    fmt.Printf("Alas, there's been an error %v", err)
-    os.Exit(1)
-  }
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error %v", err)
+		os.Exit(1)
+	}
 }
