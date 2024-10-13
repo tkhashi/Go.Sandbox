84c068a50dd959d112bd402f3b32cb321896856c
tkhashi <pupodenews@gmail.com>
2024-10-13 15:13:20 +0900

    refactor

--- tree-interactive/main.go
+++ tree-interactive/main.go
@@ -1,37 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"io/fs"
-	"log"
 	"os"
+	"path/filepath"
+
+	gotree "github.com/disiqueira/gotree"
 )
 
-// 現在のパスを返す
-func getCurrentPath() string {
-	pwd, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	fmt.Println(pwd)
-
-	return pwd
+type Folder struct {
+	path string
 }
 
-// dir配下のファイル、ディレクトリを返す
-func listFiles(dir string) []string {
-	root := os.DirFS(dir)
+type File struct {
+	path string
+}
 
-	mdFiles, err := fs.Glob(root, "*")
+func GetChildren(root string, tree gotree.Tree) (gotree.Tree, error) {
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		tree.Add(path)
+		return nil
+	})
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	var files []string
-	for _, v := range mdFiles {
-		files = append(files, v)
-		fmt.Println(v)
+	return tree, nil
+}
+
+func main() {
+	root, err := os.Getwd()
+	if err != nil {
+		return
 	}
-	return files
+
+	tree := gotree.New(root)
+
+	children, err := GetChildren(root, tree)
+
+	println(children.Print())
 }
